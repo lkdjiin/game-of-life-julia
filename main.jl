f06aedb8f55e234059d3042c464f6c6b58f825dc
mguzmann <mortem.dei@gmail.com>
2015-05-01 19:30:32 +0200

    set characters for display from the main function

--- main.jl
+++ main.jl
@@ -3,13 +3,14 @@ include("cell.jl")
 include("generation.jl")
 include("display.jl")
 
-function main(height, width, number_of_generation)
+function main(height, width, number_of_generation, sym="@", noll=" ")
+    ## print(number_of_generation)
     generation = Generation.create(height, width)
     for _ in 1:number_of_generation
-        Display.draw(generation)
+        Display.draw(generation, sym, noll)
         sleep(0.8)
         generation = Generation.next(generation)
     end
 end
 
-main(20, 80, 1)
+main(20, 80, 20, "*", " ")
